0521f09d227198a6363b2177484b304f5c07274f
estebanalmiron1@gmail.com <estebanalmiron1@gmail.com>
2025-02-24 19:12:36 -0300

    Hot fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import confetti from 'canvas-confetti'
 
 import { Square } from './components/Square.jsx'
-import { TURNS} from './constants.js'
+import { TURNS } from './constants.js'
 import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import { WinnerModal } from './components/WinnerModal.jsx'
 import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
